2d0058b694139ac1c3917d9fb5f44f29b984e1a5
Ivar <ivarsarner@gmail.com>
2015-05-29 01:44:38 +0200

    controls

--- js/internat.js
+++ js/internat.js
@@ -1,21 +1,40 @@
 $(document).ready(function(){
+var langSwitch = false;
 
+	$("#btn-lang").click(function() {
+		
+		if (langSwitch === false) {
+			i18n.init({ lng: "sv" }, function(t) {
+  			$(".controls").i18n();
+  			var appName = t("lang.type");
+  			
+			});
+			langSwitch = true;
+		} else if (langSwitch === true) {
+			i18n.init({ lng: "en" }, function(language) {
+  			$(".controls").i18n();
+  			var appName = language("lang.type");
+		});
+			langSwitch = false;
+		}
+	});
+
+/*
 	$("#btn-lang-en").click(function() {
 				
 		i18n.init({ lng: "en" }, function(language) {
-  			$(".shortcuts").i18n();
+  			$(".controls").i18n();
   			var appName = language("lang.type");
-  			alert(appName);
 		});
 	});
 
 	$("#btn-lang-sv").click(function() {
 				
 		i18n.init({ lng: "sv" }, function(t) {
-  			$(".message").i18n();
+  			$(".controls").i18n();
   			var appName = t("lang.type");
-  			alert(appName);
+  			
 		});
 	});
-	
+	*/
 });
